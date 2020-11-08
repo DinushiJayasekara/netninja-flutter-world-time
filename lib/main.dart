0c06a9a3eae564ff24a9fcf342f719b9643ca2cf
DinushiJayasekara <jkdrj02@gmail.com>
2020-11-08 20:57:59 +0530

    add-worldtime-api

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:netninja_flutter_world_time/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/home',
+    initialRoute: '/',
     routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,25 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response =
+        await get('http://worldtimeapi.org/api/timezone/Asia/Colombo');
+    Map data = jsonDecode(response.body);
+
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
